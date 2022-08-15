efb371c302747457e4ca4d30af2d40cc895bf049
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-08-15 20:35:44 -0300

    Adicionando items da cesta

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,16 +1,48 @@
-import logo from "../../assets/logo.png";
+import logo from '../../assets/logo.png';
+
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brocolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abobora.png';
 
 const cesta = {
-    topo: {
-        titulo: "Detalhe da Cesta",
-    },
-    detalhe: {
-        nome: "Cesta de Verduras",
-        logoFazenda: logo,
-        nomeFazenda: "Jenny Jack Farm",
-        descricao: "Um cesta com produtos selecionados cuidadosamenteda fazenda direto para sua cozinha",
-        preco: "R$ 40,00",
-    }
+  topo: {
+    titulo: "Detalhe da Cesta",
+  },
+  detalhe: {
+    nome: "Cesta de Verduras",
+    logoFazenda: logo,
+    nomeFazenda: "Jenny Jack Farm",
+    descricao: "Um cesta com produtos selecionados cuidadosamenteda fazenda direto para sua cozinha",
+    preco: "R$ 40,00",
+    botao: "Comprar",
+  },
+  itens: {
+    titulo: "Itens da cesta",
+    lista: [
+      {
+        nome: "Tomate",
+        imagem: tomate,
+      },
+      {
+        nome: "Brócolis",
+        imagem: brocolis,
+      },
+      {
+        nome: "Batata",
+        imagem: batata,
+      },
+      {
+        nome: "Pepino",
+        imagem: pepino,
+      },
+      {
+        nome: "Abóbora",
+        imagem: abobora,
+      }
+    ]
+  }
 }
 
 export default cesta;
